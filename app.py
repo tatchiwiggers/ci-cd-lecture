7486413df2157d1544111a460c88b4b0755d9123
tabatha wiggers <tatchiwiggers@gmail.com>
2024-08-17 19:54:27 +0000

    add welcome message

--- app.py
+++ app.py
@@ -4,4 +4,5 @@ from api.fast import root
 
 st.write(root())
 
+st.write('Welcome batch 1643')
 st.write('This is obviously very simple, in reality one would rather use this page to display API results more nicely! :)')
